fd6dda92107b860edc185d23b17c22504124c85b
Gamerman2001 <Tro12185@gmail.com>
2020-01-31 18:38:38 -0500

    adding menu items and logo to header Nav

--- src/containers/HeaderNav/HeaderNav.js
+++ src/containers/HeaderNav/HeaderNav.js
@@ -1,14 +1,21 @@
-import React, { Component } from 'react'
-import {Image, Menu} from 'semantic-ui-react'
-import './HeaderNav.scss' 
-import logo from '../../assets/images/TheTube.png'
+import React, { Component } from "react";
+import { Image, Menu } from "semantic-ui-react";
+import "./HeaderNav.scss";
+import logo from "../../assets/images/TheTube.png";
 
 export default class HeaderNav extends Component {
   render() {
     return (
       <div>
-        
+        <Menu borderless className="top-menu" fixed="top">
+          <Menu.item header className="logo">
+            <image src={logo} size="tiny" />
+          </Menu.item>
+          <Menu.menu>
+
+          </Menu.menu>
+        </Menu>
       </div>
-    )
+    );
   }
 }
