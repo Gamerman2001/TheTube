381ec446d56d96529ca100972535ad09f358b197
Gamerman2001 <Tro12185@gmail.com>
2020-01-31 18:25:39 -0500

    adding header nav class component

---
+++ src/containers/HeaderNav/HeaderNav.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import {Image, Menu} from 'semantic-ui-react'
+import './HeaderNav.scss' 
+import logo from '../../assets/images/TheTube.png'
+
+export default class HeaderNav extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
