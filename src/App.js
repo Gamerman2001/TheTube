d2cf88fd6d78cccf00a8edfbf6b3968f85f4df28
Gamerman2001 <Tro12185@gmail.com>
2020-02-09 11:10:33 -0500

    adding sidebar items, put sidebar into APP.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.scss';
 import HeaderNav from './containers/HeaderNav/HeaderNav'
+import SideBar from './containers/SideBar/SideBar'
 
 function App() {
   return (
     <div >
       <HeaderNav />
+      <SideBar />
     </div>
   );
 }
--- src/containers/Sidebar/Sidebar.js
+++ src/containers/Sidebar/Sidebar.js
@@ -1,1 +1,14 @@
-import '.Sidebar.scss'
+import React, { Component } from 'react'
+import './SideBar.scss'
+import {SideBarItem} from './SideBarItem/SideBarItem'
+import {Menu} from 'semantic-ui-react'
+
+export default class SideBar extends Component {
+  render() {
+    return (
+      <Menu borderless vertical stackable fixed='left' className='side-nav'>
+        
+      </Menu>
+    )
+  }
+}
