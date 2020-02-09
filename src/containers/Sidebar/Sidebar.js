31cc840bfc79c6b8b56092c715a09d7ee2f636ef
Gamerman2001 <Tro12185@gmail.com>
2020-02-09 13:03:16 -0500

    added side bar items

--- src/containers/Sidebar/Sidebar.js
+++ src/containers/Sidebar/Sidebar.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react'
 import './SideBar.scss'
-import {SideBarItem} from './SideBarItem/SideBarItem.js'
+import SideBarItem from './SideBarItem/SideBarItem'
 import {Menu} from 'semantic-ui-react'
 
 export default class SideBar extends Component {
   render() {
     return (
       <Menu borderless vertical stackable fixed='left' className='side-nav'>
-
+        <SideBarItem />
+        <SideBarItem />
+        <SideBarItem />
+        <SideBarItem />
+        <SideBarItem />
+        <SideBarItem />
+        <SideBarItem />
+        <SideBarItem />
+        <SideBarItem />
+        <SideBarItem />
       </Menu>
     )
   }
