6ccc99a3171ac56c7ab85a92a61be89f00c19553
Gamerman2001 <Tro12185@gmail.com>
2020-02-29 11:06:20 -0500

    added subscriptiion and subscriptions component

---
+++ src/containers/Sidebar/Subscriptions/Subscriptions.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import {Subscription} from './Subscription/Subscription'
+import {Divider} from 'semantic-ui-react'
+import {SideBarHeader} from '../SideBarHeader/SideBarHeader'
+
+export class Subscriptions extends Component {
+  render() {
+    return (
+      <>
+      <SideBarHeader title='subscriptons'/>
+      </>
+    )
+  }
+}
