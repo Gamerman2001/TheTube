07cb73947f8a1e503b0c23667156fb06936a3dab
Gamerman2001 <Tro12185@gmail.com>
2020-03-19 23:27:45 -0400

    adding home component

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,47 @@
-import React from 'react';
 import { render } from '@testing-library/react';
-import {shallow} from 'enzyme'
 import {HeaderNav} from './containers/HeaderNav/HeaderNav'
 import {SideBar} from './containers/SideBar/SideBar'
+import { create } from "react-test-renderer";
+import React from 'react';
+import {shallow} from 'enzyme'
 import {App} from './App';
 
 test('Renders App component', () => {
   const wrapper = shallow(
     <App />
   )
-  // console.log(wrapper)
   expect(wrapper).toMatchSnapshot();
+});
+
+class Button extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: "" };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(() => {
+      return { text: "PROCEED TO CHECKOUT" };
+    });
+  }
+
+  render() {
+    return (
+      <button onClick={this.handleClick}>
+        {this.state.text || this.props.text}
+      </button>
+    );
+  }
+}
+
+describe("Button component", () => {
+  test("it shows the expected text when clicked (testing the wrong way!)", () => {
+    const component = create(<Button text="SUBSCRIBE TO BASIC" />);
+    const instance = component.root;
+    console.log( component.root.props)
+    const button = instance.findByType("button");
+    button.props.onClick();
+    expect(button.props.children).toBe("PROCEED TO CHECKOUT");
+  });
 });
