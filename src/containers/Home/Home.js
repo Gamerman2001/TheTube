09bb1ee29e5b0b2ec50bd470715fd38edfdefff2
Gamerman2001 <Tro12185@gmail.com>
2020-03-20 03:15:49 -0400

    added styling for home, placed mock video previews

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,13 +1,21 @@
-import React, { Component } from 'react'
-import {VideoPreview} from '../../components/VideoPreview/VideoPreview'
-import './Home.scss'
+import React, { Component } from "react";
+import { VideoGrid } from "../../components/VideoGrid/VideoGrid";
+import { VideoPreview } from "../../components/VideoPreview/VideoPreview";
+import "./Home.scss";
+import { SideBar } from "../SideBar/SideBar";
 
 export class Home extends Component {
   render() {
     return (
-      <div class='home'>
-        <VideoPreview />
-      </div>
-    )
+      <>
+        <div class="home">
+          <SideBar />
+          <div className="responsive-video-grid-container">
+            <VideoGrid title="Trending" />
+            <VideoGrid title="Autos & Vehicles" hideDivider={true} />
+          </div>
+        </div>
+      </>
+    );
   }
 }
