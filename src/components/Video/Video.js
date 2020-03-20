74e9527ecfa328baf039c62ced8c6ef4b8a0e81b
Gamerman2001 <Tro12185@gmail.com>
2020-03-20 19:53:54 -0400

    refactored video to css height width

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -1,18 +1,28 @@
-import React from 'react'
-import './Video.scss'
+import React from "react";
+import "./Video.scss";
 
 export function Video(props) {
-  const BASE_EMBED_URL = 'https://www.youtube.com/embed/';
+  const BASE_EMBED_URL = "https://www.youtube.com/embed/";
 
-  if(!props.id){
-    return null 
+  if (!props.id) {
+    return null;
   }
 
   const embedUrl = `${BASE_EMBED_URL}${props.id}`;
   return (
     <>
-    <iframe width={'560'} title='video' height={'315'} src={embedUrl} frameBorder='0'
-            allow='autoplay; encrypted-media' allowFullScreen />
+      <div className="video-container">
+        <div className="video">
+          <iframe
+            className='video-player'
+            title="video"
+            src={embedUrl}
+            frameBorder="0"
+            allow="autoplay; encrypted-media"
+            allowFullScreen
+          />
+        </div>
+      </div>
     </>
-  )
+  );
 }
--- src/components/Watch/Watch.js
+++ src/components/Watch/Watch.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import './Watch.scss'
+import {Video} from '../Video/Video'
 
 export class Watch extends Component {
   render() {
     return (
-      <div>
-        Watch Component
+      <div style={{maxWidth: "80%"}} className='CHECKTHISS!!!'>
+        <Video id='-7fuHEEmEjs'/>
       </div>
     )
   }
