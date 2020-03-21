db6fcf892699112be6d4fb6299bf38fe22353939
Gamerman2001 <Tro12185@gmail.com>
2020-03-20 23:49:48 -0400

    added horizontal video preview and tests

--- src/components/Watch/Watch.js
+++ src/components/Watch/Watch.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import './Watch.scss'
 import {Video} from '../Video/Video'
+import {VideoPreview} from '../VideoPreview/VideoPreview'
 
 export class Watch extends Component {
   render() {
     return (
       <div style={{maxWidth: "80%"}} className='CHECKTHISS!!!'>
         <Video id='-7fuHEEmEjs'/>
+        <VideoPreview horizontal={true}/>
       </div>
     )
   }
