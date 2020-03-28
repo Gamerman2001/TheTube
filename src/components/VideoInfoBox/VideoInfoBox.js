63df8d017dea4203f6d35d30a97c64c0b617322a
Gamerman2001 <Tro12185@gmail.com>
2020-03-28 19:14:29 -0400

    added hiding video desciption to Video Info Box

--- src/components/VideoInfoBox/VideoInfoBox.js
+++ src/components/VideoInfoBox/VideoInfoBox.js
@@ -1,33 +1,61 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import {Image, Button} from 'semantic-ui-react'
+import React, { Component, useState } from "react";
+import PropTypes from "prop-types";
+import { Image, Button } from "semantic-ui-react";
+import "./VideoInfoBox.scss";
 
 export class VideoInfoBox extends Component {
- 
+  state = {
+    collapsed: false
+  };
 
+  onToggleCollapseButtonClick = () => {
+    this.setState(prevState => {
+      return {
+        collapsed: !prevState.collapsed
+      };
+    });
+  };
   render() {
+    let descriptionTextClass = "collapsed";
+    let buttonTitle = "Show More";
+    if(!this.state.collapsed) {
+      descriptionTextClass = 'expanded'
+      buttonTitle = 'Show Less'
+    }
     return (
       <div className="video-info-box">
-        <Image className="channel-image" src="http://via.placeholder.com/48x48" circular/>
+        <Image
+          className="channel-image"
+          src="http://via.placeholder.com/48x48"
+          circular
+        />
         <div className="video-info">
           <div className="channel-name">channel name</div>
           <div className="video-publication-date"> 04 10 1999</div>
         </div>
-        <Button color='youtube'>91.5k to subscribe</Button>
-        <div className="video-description">
+        <Button color="youtube">91.5k to subscribe</Button>
+        <div className="video-description"
+        >
+          <div className={descriptionTextClass}>
           <p>paragraph 1</p>
           <p>paragraph 2</p>
           <p>paragraph 3</p>
           <p>paragraph 4</p>
           <p>paragraph 5</p>
-          <Button compact>show more</Button>
+          </div>
+          
+          <Button
+            compact
+            onClick={() => {
+              this.onToggleCollapseButtonClick();
+            }}
+          >
+            {buttonTitle}
+          </Button>
         </div>
       </div>
-    )
+    );
   }
 }
 
-
-
-
-
+console.log(this, "inside video info box");
