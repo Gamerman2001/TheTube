26bbed43e47e9974d86fc1a4242302129a003c8e
Gamerman2001 <Tro12185@gmail.com>
2020-04-01 17:14:40 -0400

    added store, added redux-saga, added configureStore

---
+++ src/store/configureStore.js
@@ -0,0 +1,14 @@
+import { applyMiddleware, createStore } from "redux";
+import reducer from "./reducers";
+import createSagaMiddleware from "redux-saga";
+import rootSaga from "./sagas";
+
+export function configureStore() {
+  const sagaMiddleware = createSagaMiddleware
+  const store = createStore(
+    reducer,
+    applyMiddleware(sagaMiddleware)
+  )
+
+  sagaMiddleware.arguments(rootSaga)
+}
