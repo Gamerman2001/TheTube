2419980debec5631b4b2a1c07a25a142c4720647
Gamerman2001 <Tro12185@gmail.com>
2020-04-04 19:46:28 -0400

    updated App.js to have redux connect and incorporate google API script

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.scss";
 // import {HeaderNav} from './containers/HeaderNav/HeaderNav'
 // import {SideBar} from './containers/SideBar/SideBar'
 import { AppLayout } from "./components/AppLayout/AppLayout";
 import { Home } from "./containers/Home/Home";
-import {Route, Switch} from 'react-router-dom'
-import {Watch} from '../src/components/Watch/Watch'
+import { Route, Switch } from "react-router-dom";
+import { Watch } from "../src/components/Watch/Watch";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import { youtubeLibraryLoaded } from "./store/actions/api";
 
-export function App() {
-  return (
-    <div >
-      <AppLayout >
-        <Switch >
-          <Route path='/watch' component={Watch} />
-          <Route path='/' component={Home} />
-        </Switch>
-      </AppLayout>
-    </div>
-  );
+const API_KEY = "";
+
+class App extends Component {
+  componentDidMount() {
+    this.loadYoutubeApi();
+  }
+  render() {
+    return (
+      <div>
+        <AppLayout>
+          <Switch>
+            <Route path="/watch" component={Watch} />
+            <Route path="/" component={Home} />
+          </Switch>
+        </AppLayout>
+      </div>
+    );
+  }
+
+  loadYoutubeApi() {
+    const script = document.createElement("script");
+    script.src = "https://apis.google.com/js/client.js";
+
+    script.onLoad = () => {
+      window.gapi.load("client", () => {
+        window.gapi.client.setApiKey(API_KEY);
+        window.gapi.client.load("youtube", "v3", () => {
+          this.props.youtubeLibraryLoaded();
+        });
+      });
+    };
+
+    document.body.appendChild(script);
+  }
 }
 
-export default App;
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ youtubeLibraryLoaded }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(App);
